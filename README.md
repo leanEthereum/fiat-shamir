af5a792f3109c8acb98fb7694bd00f34c13a6735
Tom Wambsgans <TomWambsgans@users.noreply.github.com>
2025-09-25 22:01:03 +0400

    setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# fiat-shamir
+# Fiat Shamir
+
+Wrappers around [Plonky3](https://github.com/Plonky3/Plonky3) Fiat-Shamir, developped in [whir-p3](https://github.com/tcoratger/whir-p3).
---
+++ src/lib.rs
@@ -0,0 +1,45 @@
+use p3_challenger::{DuplexChallenger, FieldChallenger, GrindingChallenger};
+use p3_field::Field;
+use p3_koala_bear::{KoalaBear, Poseidon2KoalaBear};
+
+mod errors;
+pub use errors::*;
+
+mod prover;
+pub use prover::*;
+
+mod verifier;
+pub use verifier::*;
+
+mod utils;
+pub use utils::*;
+
+const LEAN_ISA_VECTOR_LEN: usize = 8;
+
+pub trait ChallengeSampler<F> {
+    fn sample(&mut self) -> F;
+
+    fn sample_vec(&mut self, len: usize) -> Vec<F>;
+
+    fn sample_bits(&mut self, bits: usize) -> usize;
+}
+
+pub trait FSChallenger<EF: Field>:
+    FieldChallenger<PF<EF>> + GrindingChallenger<Witness = PF<EF>> + ChallengerState
+{
+}
+
+impl<F: Field, C: FieldChallenger<PF<F>> + GrindingChallenger<Witness = PF<F>> + ChallengerState>
+    FSChallenger<F> for C
+{
+}
+
+pub trait ChallengerState {
+    fn state(&self) -> String;
+}
+
+impl ChallengerState for DuplexChallenger<KoalaBear, Poseidon2KoalaBear<16>, 16, 8> {
+    fn state(&self) -> String {
+        format!("{:?}", self.sponge_state)
+    }
+}
