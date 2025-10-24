211b12c35c9742c3d2ec0477381954208f97986c
Tom Wambsgans <TomWambsgans@users.noreply.github.com>
2025-10-24 11:37:23 +0400

    FPacking

--- src/wrappers.rs
+++ src/wrappers.rs
@@ -3,6 +3,7 @@ use p3_field::{ExtensionField, PrimeCharacteristicRing};
 use crate::*;
 
 pub type PF<F> = <F as PrimeCharacteristicRing>::PrimeSubfield;
+pub type FPacking<F> = <F as Field>::Packing;
 pub type PFPacking<F> = <PF<F> as Field>::Packing;
 pub type EFPacking<EF> = <EF as ExtensionField<PF<EF>>>::ExtensionPacking;
 
